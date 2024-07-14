816fbba0d4c54d5ce4a09245b9b40b059b03305c
Sharook Mohamed <sharookmohamed007@gmail.com>
2024-07-14 11:44:46 +0530

    Prepare for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header";
 import CurrencyConverter from "./components/CurrencyConverter/CurrencyConverter";
 import "./index.css";
-
+import React from 'react';
 function App() {
   return (
     <>
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const heading: string = "currency converter";
 const API_DOMAIN: string = "https://v6.exchangerate-api.com/v6";
-const API_KEY: string = "USE-YOUR-API-KEY";
+const API_KEY: string = "e10887b39f3b8367b13c43b9";
 export const endpointPath = (from: string): string =>
   `${API_DOMAIN}/${API_KEY}/latest/${from}`;
